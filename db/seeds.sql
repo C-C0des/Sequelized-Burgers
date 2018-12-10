5e10e96f60e8dc139dd93341ca3b83a8038831b1
Diaspar <the.diaspar@gmail.com>
2018-12-10 00:20:53 -0500

    complete

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Philly Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("NY Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicago Burger", false);
